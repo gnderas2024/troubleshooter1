7091e85205032a688bde5840d11a897901d7a10f
gnderas2024 <techupto2021@gmail.com>
2024-07-26 10:01:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Variables para almacenar las respuestas
+var responses = {
+    yes: 0,
+    no: 0
+};
+
+// Función para actualizar el gráfico
+function updateChart() {
+    var ctx = document.getElementById('responsesChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Sí', 'No'],
+            datasets: [{
+                label: 'Número de Respuestas',
+                data: [responses.yes, responses.no],
+                backgroundColor: ['#36a2eb', '#ff6384']
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
 document.getElementById('next-button').addEventListener('click', function() {
     console.log("Botón 'Next' clickeado");
     // Lógica para moverse al siguiente paso basado en las respuestas
@@ -23,12 +52,14 @@ function loadQuestion(step) {
         var yesButton = document.createElement('button');
         yesButton.innerText = 'Sí';
         yesButton.onclick = function() {
+            responses.yes++;
             nextStep(step + 1);
         };
 
         var noButton = document.createElement('button');
         noButton.innerText = 'No';
         noButton.onclick = function() {
+            responses.no++;
             nextStep(step + 1);
         };
 
@@ -37,6 +68,8 @@ function loadQuestion(step) {
         container.appendChild(noButton);
     } else {
         container.innerHTML = '<p>¡Gracias por completar el solucionador de problemas!</p>';
+        // Actualizar el gráfico con las respuestas finales
+        updateChart();
     }
 }
 
