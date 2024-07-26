8231f4443414141f69cbbd6b7d56bbfcce2fa678
gnderas2024 <techupto2021@gmail.com>
2024-07-26 09:53:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 document.getElementById('next-button').addEventListener('click', function() {
+    console.log("Botón 'Next' clickeado");
     // Lógica para moverse al siguiente paso basado en las respuestas
-  });
-  
-  function loadQuestion(step) {
+});
+
+function loadQuestion(step) {
+    console.log("Cargando pregunta para el paso: " + step);
     var container = document.getElementById('question-container');
     container.innerHTML = '';  // Limpiar el contenedor
-  
+
     var questions = [
-      "¿Está el cable de alimentación conectado correctamente?",
-      "¿Probaste con otro cable de alimentación?",
-      "¿Funciona el enchufe donde está conectado el equipo?",
-      "¿Revisaste la fuente de alimentación del equipo?",
-      "¿Intentaste encender el equipo con una batería nueva o diferente?",
-      "Si todas las respuestas anteriores son 'Sí' y el problema persiste, por favor contacta al soporte técnico."
+        "¿Está el cable de alimentación conectado correctamente?",
+        "¿Probaste con otro cable de alimentación?",
+        "¿Funciona el enchufe donde está conectado el equipo?",
+        "¿Revisaste la fuente de alimentación del equipo?",
+        "¿Intentaste encender el equipo con una batería nueva o diferente?",
+        "Si todas las respuestas anteriores son 'Sí' y el problema persiste, por favor contacta al soporte técnico."
     ];
-  
+
     if (step < questions.length) {
-      var question = document.createElement('div');
-      question.innerHTML = '<p>' + questions[step] + '</p>';
-      var yesButton = document.createElement('button');
-      yesButton.innerText = 'Sí';
-      yesButton.onclick = function() {
-        nextStep(step + 1);
-      };
-  
-      var noButton = document.createElement('button');
-      noButton.innerText = 'No';
-      noButton.onclick = function() {
-        // Lógica para respuesta "No"
-        nextStep(step + 1);
-      };
-  
-      container.appendChild(question);
-      container.appendChild(yesButton);
-      container.appendChild(noButton);
+        var question = document.createElement('div');
+        question.innerHTML = '<p>' + questions[step] + '</p>';
+        var yesButton = document.createElement('button');
+        yesButton.innerText = 'Sí';
+        yesButton.onclick = function() {
+            nextStep(step + 1);
+        };
+
+        var noButton = document.createElement('button');
+        noButton.innerText = 'No';
+        noButton.onclick = function() {
+            nextStep(step + 1);
+        };
+
+        container.appendChild(question);
+        container.appendChild(yesButton);
+        container.appendChild(noButton);
     } else {
-      container.innerHTML = '<p>¡Gracias por completar el solucionador de problemas!</p>';
+        container.innerHTML = '<p>¡Gracias por completar el solucionador de problemas!</p>';
     }
-  }
-  
-  function nextStep(step) {
+}
+
+function nextStep(step) {
+    console.log("Siguiente paso: " + step);
     loadQuestion(step);
-  }
-  
-  // Iniciar con el primer paso
-  loadQuestion(0);
-  
+}
+
+// Iniciar con el primer paso
+loadQuestion(0);
+console.log("Iniciando el solucionador de problemas");
